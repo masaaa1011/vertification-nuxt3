79a2b69365bf27824760d1cffcfe97e83151ae85
masaaa1011 <elle.hedgehog.diaries.26masa08@gmail.com>
2022-04-05 02:01:39 +0900

    update pipeline.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     ssr: true,
+    nitro: {
+        preset: 'azure_functions'
+    }
 })
